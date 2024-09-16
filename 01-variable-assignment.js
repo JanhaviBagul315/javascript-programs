52a13bd907ff5cd3c7b1232e62fbb9586b4c9d41
JanhaviBagul315 <janhavibagul31@gmail.com>
2024-09-17 00:29:24 +0530

    Javascript Variables
    Javascript Variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,12 @@
+console.log("My city name is Nashik");
+
+console.log("Ani is my best friends");
+console.log("My country is India");
+console.log("My state is Maharashtra");
+console.log("My school name is Jaihind highschool");
+console.log("My collage name is met institute of engineering");
+
+
+
+
+
---
+++ 01-variable.js
@@ -0,0 +1,19 @@
+// variable declaration
+var firstName;
+console.log(firstName);
+
+// variable initialization
+firstName = "Jenny";
+console.log(firstName);
+
+// variable declaration and initialization
+var lastName = "Gates";
+console.log(lastName);
+
+// variable update
+firstName = "Jennifer"
+console.log(firstName);
+
+var city = "Nashik";
+console.log("City name is",city);
+
