da916bea985ebc1c735a80819ed97e27db22a75a
JanhaviBagul315 <janhavibagul31@gmail.com>
2024-09-18 18:14:18 +0530

    Variable assignments
    Variable assignments

---
+++ 01-variableAssign.js
@@ -0,0 +1,36 @@
+// 1. Declare a variable with name - myFullName
+// Log the variable myFullName value before initialization on console
+// Initialize the myFullName variable with your first and last name only. Log the value on console
+// Update the myFullName variable with your first name, middle name and last name. Log the updated value on console
+var myFullName;
+console.log(myFullName);
+myFullName ="Janhavi Bagul";
+console.log("My First and Last Name is :", myFullName);
+myFullName = "Janhavi Bhausaheb Bagul";
+console.log("My Full Name is :", myFullName);
+
+// 2.Declare and initialize a variable with name 'myLove with value → "I love only JS" and log result on console
+var myLove = "I love only JS";
+
+// 3. Create a variable for form controls as listed below. Declare and initialize must be on same line
+// firstName
+// lastName
+// age
+// address
+// pinCode
+// gender
+// IsMarried
+var firstName = "Janhavi";
+console.log("My Firstname is :", firstName);
+var lastName = "Bagul";
+console.log("My Lastname is :", lastName);
+var age = 22;
+console.log("my age is :", age);
+var address = "Nashik";
+console.log("I Am from",address);
+var pincode = 422004;
+console.log("My city picode is :",pincode);
+var gender = "Female";
+console.log("My Gender is :", gender);
+var isMarried = false;
+console.log("My married status is :", isMarried);
---
+++ 02-typeOf.js
@@ -0,0 +1,26 @@
+// 1 Declare variable bankName and assign value 'SBI Bank' separately also log value on console before and after initialization along with it's data type using typeof operator.
+var bankName ;
+console.log("Before initialization",bankName , "And Datatype is :", typeof bankName);
+bankName = "SBI Bank";
+console.log("After initialization",bankName, "And Datatype is :",typeof bankName );
+
+// 2. On the same line Declare and initialize a variable with name marks = '90%' Log it's datatype and value on console using typeof operator.
+var marks = '90%';
+console.log("Datatype is :", marks , "Datatype is :",typeof marks);
+
+// 3. Declare and initialize a variable separately isWorking with true but log value before and after initialization on console along with it's data type
+var isWorking;
+console.log("Before Initialization", isWorking , "And Datatype is :", typeof isWorking);
+isWorking = true;
+console.log("After Initialization", isWorking , "And Datatype is :", typeof isWorking);
+
+// 4. Declare and initialize a variable on same line as totalCount= "Hundred and Seven",
+var totalCount = "Hundred and Seven";
+
+// a. Log the value on console as it is, along with it's data type
+console.log("Total Count is :", totalCount, "And Datatype is :", typeof totalCount);
+
+// b. Update variable totalCount with new value totalCount=107, Log the value and it's data type on console
+var totalCount = 107;
+console.log("Total count is :",totalCount , "And Datatype is :",typeof totalCount );
+
